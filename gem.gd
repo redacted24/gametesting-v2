71d9d203a757d5901dea139baa1c1ef6f2f8bf00
Qiuyu Huang <qiu.y.huang@mail.mcgill.ca>
2024-09-19 12:25:52 -0400

    merge: desktop and laptop

--- gem.gd
+++ gem.gd
@@ -3,6 +3,11 @@ extends Area3D
 signal collected
 
 func _on_body_entered(body: Node3D) -> void:
+<<<<<<< Updated upstream
 	print('coin collected')
 	collected.emit()
 	queue_free()
+=======
+    print('coin collected')
+    queue_free()
+>>>>>>> Stashed changes
--- player.gd
+++ player.gd
@@ -11,59 +11,59 @@ var nb_jumps = 0
 var delay_count = 0
 
 func _physics_process(delta: float) -> void:
-	var direction = Vector3.ZERO
-	
-	if Input.is_action_pressed('move_up'):
-		direction.z -= 1
-		direction.x -= 1
-	if Input.is_action_pressed('move_down'):
-		direction.z += 1
-		direction.x += 1
-	if Input.is_action_pressed('move_left'):
-		direction.x -= 1
-		direction.z += 1
-	if Input.is_action_pressed('move_right'):
-		direction.x += 1
-		direction.z -= 1
-		
-	# Reset jump counter when on floor
-	if is_on_floor() and nb_jumps != 0:
-		jumping = false
-		nb_jumps = 0
-		
-	# First jump
-	if is_on_floor() and Input.is_action_pressed('jump'):
-		target_velocity.y = jump_strength
-		nb_jumps += 1
-		jumping = true
+    var direction = Vector3.ZERO
+    
+    if Input.is_action_pressed('move_up'):
+        direction.z -= 1
+        direction.x -= 1
+    if Input.is_action_pressed('move_down'):
+        direction.z += 1
+        direction.x += 1
+    if Input.is_action_pressed('move_left'):
+        direction.x -= 1
+        direction.z += 1
+    if Input.is_action_pressed('move_right'):
+        direction.x += 1
+        direction.z -= 1
+        
+    # Reset jump counter when on floor
+    if is_on_floor() and nb_jumps != 0:
+        jumping = false
+        nb_jumps = 0
+        
+    # First jump
+    if is_on_floor() and Input.is_action_pressed('jump'):
+        target_velocity.y = jump_strength
+        nb_jumps += 1
+        jumping = true
 
-	# Double jump
-	if nb_jumps < 2 and Input.is_action_pressed('jump') and delay_count > jump_delay:
-		target_velocity.y = jump_strength
-		nb_jumps = 0
-		delay_count = 0
-		jumping = false
-	
-	# Jump delay counter
-	if jumping == true:
-		delay_count += delta
+    # Double jump
+    if nb_jumps < 2 and Input.is_action_pressed('jump') and delay_count > jump_delay:
+        target_velocity.y = jump_strength
+        nb_jumps = 0
+        delay_count = 0
+        jumping = false
+    
+    # Jump delay counter
+    if jumping == true:
+        delay_count += delta
 
-	# Normalize
-	# Set rotation
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$Pivot.basis = Basis.looking_at(direction, Vector3.UP, true)
-		
-		# Ground velocity
-		target_velocity.x = direction.x * speed
-		target_velocity.z = direction.z * speed
+    # Normalize
+    # Set rotation
+    if direction != Vector3.ZERO:
+        direction = direction.normalized()
+        $Pivot.basis = Basis.looking_at(direction, Vector3.UP, true)
+        
+        # Ground velocity
+        target_velocity.x = direction.x * speed
+        target_velocity.z = direction.z * speed
 
-	if not is_on_floor():
-		target_velocity.y = target_velocity.y - (fall_acceleration*delta)
-	
-	velocity = target_velocity
-	target_velocity = Vector3.ZERO
-	move_and_slide()
-	
-	# Camera Smoothness
-	$CameraControl.position = lerp($CameraControl.position,position,0.15)
+    if not is_on_floor():
+        target_velocity.y = target_velocity.y - (fall_acceleration*delta)
+    
+    velocity = target_velocity
+    target_velocity = Vector3.ZERO
+    move_and_slide()
+    
+    # Camera Smoothness
+    $CameraControl.position = lerp($CameraControl.position,position,0.15)
